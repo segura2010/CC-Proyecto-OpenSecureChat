61cf7a0acc7433f3237face9b85dc74b4b877b98
alberto__segura <alberto.segura.delgado@gmail.com>
2015-10-28 20:13:41 +0100

    adding libs and user controller

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 
-// Librerias mongo
+// Get config file (config.json)
+var config = require('./config.json');
+
+// Libs for MongoDB
 var mongo = require('mongodb');
 var monk = require('monk');
-// Inicializacion dbmongo
-var dbmongo = monk('localhost:27017/opensecurechat');
 
-// Inicializacion redis
+// Libs for RedisDB
 var redis = require("redis");
-var client = redis.createClient();
 
-var pruebas = dbmongo.get("pruebas");
+// Libs for HTTP Server (Web)
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+// Libs for SocketIO (using the http server)
+var io = require('socket.io')(http);
+
+// Initialize dbmongo
+var dbmongo = monk('localhost:27017/opensecurechat');
+// Initialize redis
+//var client = redis.createClient();
+
+// Get DB
+var users = dbmongo.get("users");
+
+
+// Initialize HTTP Server
+app.use(express.static('public_html'));
+// Listen on port
+http.listen(config.httpServer.port, function(){
+	console.log('listening on *:'+ config.httpServer.port);
+});
 
