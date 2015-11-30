ce3e08ad4cf856044c23fdf0ccec6f7bcfabcb64
alberto__segura <alberto.segura.delgado@gmail.com>
2015-11-30 20:01:37 +0100

    added bash script with commands to setup your deployment on heroku

---
+++ deploy_setup.sh
@@ -0,0 +1,19 @@
+# Si no habias iniciado sesion ya, necesitaras hacerlos
+heroku login;
+
+# Crea la app en Heroku, debes  tener el cliente instalado y configurado
+heroku create;
+
+# Valores para variables de entorno necesarias (local)
+export KEY_SIZE=2048;
+export MONGODB_URL="URL_BASE_DATOS_MONGO";
+export REDIS_URL="URL_BASE_DATOS_REDIS";
+export PORT=3000;
+
+# Valores para variables de entorno necesarias (Heroku), a partir de las locales
+heroku config:set KEY_SIZE=$KEY_SIZE;
+heroku config:set MONGODB_URL=$MONGODB_URL;
+heroku config:set REDIS_URL=$REDIS_URL;
+
+# Estas variables tambien se puede ver el valor que contienen con comandos como:
+heroku config:get KEY_SIZE;
